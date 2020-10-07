8733034ebe75b09aa2fc7508c889ed71dab96c9b
Markay12 <ashinhust.brass@gmail.com>
2020-10-07 11:06:54 -0700

    updated new demo code

---
+++ demoCode/helloW.c
@@ -0,0 +1,16 @@
+//pre compiler directives
+#include<stdio.h>
+#define TEST 100
+
+//usually where prototypes go
+
+//global scope
+const float PI = 3.141592673;
+
+int main(int argc, char** argv)
+{
+
+    printf("Hello World!");
+
+}
+
